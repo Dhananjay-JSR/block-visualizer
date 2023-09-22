ac20b780989467c6680775de30fdb056dd98e92f
Dhananjay-JSR <ajaysenday@gmail.com>
2023-09-22 21:02:52 +0530

    Updated Node Info

--- src/routes/Graph.tsx
+++ src/routes/Graph.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "./Main";
 import DATARAW from '../assets/bc1qfqqed76qxqm2epmxlv7ywgjz8k6tk472pj7msn.json'
-import ReactFlow, { useNodesState, useEdgesState, addEdge, Edge, Node, Controls, MiniMap, Background, Connection, Handle, Position } from 'reactflow';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, Edge, Node, Controls, MiniMap, Background, Connection, Handle, Position, Panel, useReactFlow, getRectOfNodes, getTransformForBounds } from 'reactflow';
 
 import 'reactflow/dist/style.css';
+import { toPng } from "html-to-image";
 // const initialNodes = [
 //     { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
 //     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
@@ -31,17 +32,17 @@ export default function GraphExplorer() {
     //   </div>
 
     const Elements = Array(5).fill(0).map((_, i) => ({
-        id: `${i+1}`,
-        data: { label: `Node ${i+1}` },
+        id: `${i + 1}`,
+        data: { label: `Node ${i + 1}` },
         position: {
             x: Math.random() * window.innerWidth - 100,
             y: Math.random() * window.innerHeight,
         },
     }));
     let CenterNodeX = 350;
-    let        CenterNodeY = 150;
+    let CenterNodeY = 150;
     const [data, setData] = useState<any>(null)
-    
+
     React.useEffect(() => {
         // fetch(`https://blockchain.info/rawaddr/${addr}`).then((res)=>res.json()).then((data)=>{
         //     console.log(data)
@@ -55,54 +56,54 @@ export default function GraphExplorer() {
         }, 1000)
     }, [])
     // console.log(data.address)
- 
-                // const initialNodes: Node[] = 
-                const initialNodes: Node[] = ([]
+
+    // const initialNodes: Node[] = 
+    const initialNodes: Node[] = ([]
     );
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
 
-    const initialEdges:Edge[] = [...Elements.map((_,index)=>{
-        return { id: `e${index+1}-source`, source: `${index+1}`, target: 'source' ,animated:true}
+    const initialEdges: Edge[] = [...Elements.map((_, index) => {
+        return { id: `e${index + 1}-source`, source: `${index + 1}`, target: 'source', animated: true }
     })]
-    
-    
+
+
     // );
-   
+
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-    useEffect(()=>{
+    useEffect(() => {
         // const NewData = data.txs.map((tx:any,index:number)=>{
         //     return {
         //         id: `${index+1}`,
         //         data: { label: `Node ${index+1}` },
         //     }})
         //     console.log(NewData)
-        if (data==null) return
-        let MapperData = data.txs.map((NewData,index)=>{
+        if (data == null) return
+        let MapperData = data.txs.map((NewData, index) => {
             return {
                 type: "selectorNode",
-                id: `${index+1}`,
-            data: { label: `${NewData.inputs[0].prev_out.addr}` },
-            position: {
-                x: Math.cos(2 * Math.PI * index / data.txs.length) * 170 + CenterNodeX,
-                y: Math.sin(2 * Math.PI * index / data.txs.length) * 170 + CenterNodeY,
-            },
+                id: `${index + 1}`,
+                data: { label: `${NewData.inputs[0].prev_out.addr}` },
+                position: {
+                    x: Math.cos(2 * Math.PI * index / data.txs.length) * 170 + CenterNodeX,
+                    y: Math.sin(2 * Math.PI * index / data.txs.length) * 170 + CenterNodeY,
+                },
             }
         })
-        setNodes([...MapperData,{ id: 'source', position: { x: CenterNodeX, y: CenterNodeY },type: "selectorNode", data: { label: data.address,isSource:true } }])
-        setEdges([...MapperData.map((_,index)=>{
-            return { id: `e${index+1}-source`, source: `${index+1}`, target: 'source' ,animated:true}
+        setNodes([...MapperData, { id: 'source', position: { x: CenterNodeX, y: CenterNodeY }, type: "selectorNode", data: { label: data.address, isSource: true } }])
+        setEdges([...MapperData.map((_, index) => {
+            return { id: `e${index + 1}-source`, source: `${index + 1}`, target: 'source', animated: true }
         })])
-    },[data])
-    
-    
-                const nodeTypes = useMemo(() => ({ selectorNode: CustomNodder }), []);
-   
+    }, [data])
+
+
+    const nodeTypes = useMemo(() => ({ selectorNode: CustomNodder }), []);
+
     // const onConnect = useCallback((params:Edge | Connection) => setEdges((eds) => addEdge( params   , eds)), [setEdges]);
-    
+
     const { addr } = useParams<{ addr: string }>()
     const [loading, setLoading] = useState(true)
-   
+
     if (loading)
         return (
             <>
@@ -114,7 +115,7 @@ export default function GraphExplorer() {
 
 
     // console.log(data)
-   
+
     return <div className="h-screen flex flex-col">
         <Header />
         <div className="w-full grow flex justify-center items-center ">
@@ -127,63 +128,111 @@ export default function GraphExplorer() {
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
             // onConnect={onConnect}
-            />
+            >
+
+                {/* <Panel position="top-right">top-left</Panel> */}
+                <DownloadButton />
+            </ReactFlow>
             <Controls />
-        <MiniMap />
-        {/* <Background variant="dots" gap={12} size={1} /> */}
+            <MiniMap />
+            {/* <Background variant="dots" gap={12} size={1} /> */}
 
         </div></div>
 
 
-} 
+}
 
-function CustomNodder({ data, isConnectable }:any) {
-   
+function CustomNodder({ data, isConnectable, selected }: any) {
+
     return (
         <>
-        {data.isSource==true&& <Handle
-            type="target"
-            position={Position.Top}
-            style={{ background: '#555' }}
-            onConnect={(params) => console.log('handle onConnect', params)}
-            isConnectable={isConnectable}
-          />}
-         
-          <div className="flex justify-center items-center flex-col">
+            {data.isSource == true && <Handle
+                type="target"
+                position={Position.Top}
+                style={{ background: '#555' }}
+                onConnect={(params) => console.log('handle onConnect', params)}
+                isConnectable={isConnectable}
+            />}
 
-          <div style={{
-            backgroundColor:data.isSource?"#00d4ff":"#ff0000"
-          }} className=" rounded-full h-10 w-10 flex justify-center items-center flex-col">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
-  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
-</svg>
+            <div className="relative flex justify-center items-center flex-col">
 
-          </div>
-          <div className="max-w-[52px] truncate">
-        
-        {/* saddsadsasdadsadsjkhdsahkjjhkdsajhkdsajhkdsa */}
-        <span>{
-        data.label}</span>
-        </div>
-          </div>
-          {/* <div>
+                <div style={{
+                    backgroundColor: data.isSource ? "#00d4ff" : "#ff0000"
+                }} className="group: rounded-full h-10 w-10 flex justify-center items-center flex-col">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`bi bi-person ${data.isSource? "" : "fill-white"}  `} viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                    </svg>
+
+                </div>
+                <div className={`absolute top-full  ${selected ? "bg-white border-2 p-2 rounded-md" : "max-w-[52px] truncate"}`}>
+
+                    {/* saddsadsasdadsadsjkhdsahkjjhkdsajhkdsajhkdsa */}
+                    <span>{
+                        data.label}</span>
+
+                </div>
+            </div>
+            {/* <div>
             Custom Color Picker Node: <strong>{data.color}</strong>
           </div>
           <input className="nodrag" type="color" onChange={data.onChange} defaultValue={data.color} /> */}
-          {/* <Handle
+            {/* <Handle
             type="source"
             position={Position.Right}
             id="a"
             style={{ top: 10, background: '#555' }}
             isConnectable={isConnectable}
           /> */}
-          {data.isSource==null && <Handle
-            type="source"
-            position={Position.Right}
-            id="b"
-            style={{  background: '#555' }}
-            isConnectable={isConnectable}
-          />}
+            {data.isSource == null && <Handle
+                type="source"
+                position={Position.Right}
+                id="b"
+                style={{ background: '#555' }}
+                isConnectable={isConnectable}
+            />}
         </>
     )
+}
+
+
+
+function downloadImage(dataUrl: string) {
+    const a = document.createElement('a');
+
+    a.setAttribute('download', 'tracer-graph.png');
+    a.setAttribute('href', dataUrl);
+    a.click();
+}
+
+const imageWidth = 1024;
+const imageHeight = 768;
+
+function DownloadButton() {
+    const { getNodes } = useReactFlow();
+    const onClick = () => {
+        // we calculate a transform for the nodes so that all nodes are visible
+        // we then overwrite the transform of the `.react-flow__viewport` element
+        // with the style option of the html-to-image library
+        const nodesBounds = getRectOfNodes(getNodes());
+        const transform = getTransformForBounds(nodesBounds, imageWidth, imageHeight, 0.5, 2);
+
+        toPng(document.querySelector('.react-flow__viewport') as HTMLElement, {
+            backgroundColor: 'white',
+            width: imageWidth,
+            height: imageHeight,
+            style: {
+                width: imageWidth,
+                height: imageHeight,
+                transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
+            },
+        }).then(downloadImage);
+    };
+
+    return (
+        <Panel position="top-right">
+            <button className="font-semibold border-2 p-2 rounded-md hover:bg-gray-300 hover:transition-all  transition-all" onClick={onClick}>
+                Save Graph Locally
+            </button>
+        </Panel>
+    );
 }
